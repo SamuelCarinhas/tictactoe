ac85a4a5f31ff8dca8762088a8ce0799bc4303a9
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2022-02-05 15:40:05 +0000

    MAIN: Add default socket connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+
+});
+
+io.on('connection', (socket) => {
+    console.log('Connected: ', socket.id);
+});
+
+httpServer.listen(3000, () => {
+    console.log('Server listening on port: 3000');
+});
